7bb8c2139052b7eb0fdfcd8bcf2e1ea4e46a3075
yadavsiddhant969-svg <yadavsiddhant969@gmail.com>
2026-08-23 20:12:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,25 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras import layers, models
 
 st.title("AI-Based Counter-Drone Detection System")
 st.write("Upload a radar micro-Doppler signal CSV file to classify the detected object.")
 
-model = tf.keras.models.load_model('drone_classifier_model.keras')
-classes = ["Drones", "Cars", "People"]
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', padding='same', input_shape=(11, 61, 1)),
+    layers.MaxPooling2D((2,2), padding='same'),
+    layers.Conv2D(64, (3,3), activation='relu', padding='same'),
+    layers.MaxPooling2D((2,2), padding='same'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dropout(0.3),
+    layers.Dense(3, activation='softmax')
+])
 
+model.load_weights('model.weights.h5')
+
+classes = ["Drones", "Cars", "People"]
 X_min = -130.0
 X_max = 0.0
 
